5e86412125e5f10bbc98d4b51e654682c386128f
aruna manandhar <ujwalnewa23@gmail.com>
2024-12-01 22:48:08 +0545

    data_type

--- 3_data_type.py
+++ 3_data_type.py
@@ -1,15 +1,10 @@
-num = 12
+num = 50
 name = "ujwal"
-age = "30"
-print(name)
-print(age)
-study = "master"
-profession = "business"
-income = "200000"
-business = "financing"
-annual = "2400000"
-print(study)
-print(profession)
-print(income)
-print(business)
-print(annual)
+phone = "98070"
+
+
+print(type(num))
+print(type(name))
+print(type(phone))
+
+
