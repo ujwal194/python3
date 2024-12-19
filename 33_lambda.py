9f15237040cfb330d8ea6354f490d748c7b49335
aruna manandhar <ujwalnewa23@gmail.com>
2024-12-19 16:50:14 +0545

    lambda.py

---
+++ 33_lambda.py
@@ -0,0 +1,11 @@
+add = lambda x,y:x+y
+print(add(5,3))
+
+
+number = [1,5,8,6]
+squared = list(map(lambda x:x**2,number))
+print(squared)
+
+number = [2,4,3,5]
+cube = list(map(lambda x:x**3,number))
+print(cube)
